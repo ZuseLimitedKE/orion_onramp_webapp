96b9d5b343c790e2e0ec013cc62b4fa222759257
Felista-Njeri <njeerikamau2@gmail.com>
2025-11-20 01:20:19 +0300

    feat: add useBusinesses hook for managing business api operations and index page for exporting from a single entry point

---
+++ src/hooks/businesses/index.ts
@@ -0,0 +1,8 @@
+// Export all business hooks from a single entry point
+export {
+  useBusinesses,
+  useBusiness,
+  useInvitations,
+  useIndustries,
+  businessQueryKeys,
+} from './useBusinesses'
