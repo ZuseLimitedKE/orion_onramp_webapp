89650667de2d22cd8b7c6bea1f222ce58646cb05
mbeka02 <antonymbeka@gmail.com>
2025-12-02 21:58:41 +0300

    only running tests in chrome for now

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,11 +39,11 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
+    /*    {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-
+*/
     /*{
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
